4833b2615e0f8d5624e00ea419eeb853eb030704
zor-cell <philippemanu.zorman@gmail.com>
2025-01-16 19:03:17 +0100

    updated java logic

--- src/connect4/Connector.java
+++ src/connect4/Connector.java
@@ -1,11 +1,24 @@
 package connect4;
 
+import connect4.data.Payload;
+import connect4.data.Position;
+
 public class Connector {
-    public static int[][] compute() {
-        return new int[6][7];
+    public static Payload computeBestMove() {
+        int[][] board = new int[6][7];
+
+        int i = (int) (Math.random() * 6);
+        int j = (int) (Math.random() * 7);
+        board[i][j] = 1;
+
+        return new Payload(board);
+    }
+
+    public static Payload makeMove(Position pos) {
+        return computeBestMove();
     }
 
     public static void main(String[] args) {
-        int[][] field = compute();
+
     }
 }
---
+++ src/connect4/data/Position.java
@@ -0,0 +1,15 @@
+package connect4.data;
+
+public class Position {
+    public int i;
+    public int j;
+
+    public Position(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public boolean isInRange(int rows, int cols) {
+        return i >= 0 && i < rows && j >= 0 && j < cols;
+    }
+}
