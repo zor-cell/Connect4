e60180283ea08535674cc68615044e723354ac9d
zor-cell <philippemanu.zorman@gmail.com>
2025-01-20 01:04:14 +0100

    updated board representation and started negamax implementation

--- src/connect4/data/MovePayload.java
+++ src/connect4/data/MovePayload.java
@@ -4,10 +4,16 @@ public class MovePayload {
     public int[][] board;
     public Position position;
     public GameState gameState;
+    public int score;
 
-    public MovePayload(int[][] board, Position position, GameState gameState) {
+    public MovePayload(int[][] board, Position position, GameState gameState, int score) {
         this.board = board;
         this.position = position;
         this.gameState = gameState;
+        this.score = score;
+    }
+
+    public MovePayload(int[][] board, Position position, GameState gameState) {
+        this(board, position, gameState, 0);
     }
 }
